3783737ed11caed2e672b5e4ab02a12994616f27
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-07-18 02:41:38 +0500

    Footer added in home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import FeaturedDestination from '../components/FeaturedDestination'
+import ExclusiveOffers from '../components/ExclusiveOffers'
+import Testimonial from '../components/Testimonial'
+import NewsLetter from '../components/NewsLetter'
 
 
 const Home = () => {
@@ -8,6 +11,9 @@ const Home = () => {
     <>
       <Hero />
       <FeaturedDestination />
+      <ExclusiveOffers />
+      <Testimonial />
+      <NewsLetter />      
     </>
   )
 }
