afad863a236654c1b686290975e376cc6ae24133
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-07-18 23:45:11 +0500

    All Rooms DIV completed

--- frontend/src/pages/AllRooms.jsx
+++ frontend/src/pages/AllRooms.jsx
@@ -1,36 +1,86 @@
-import React from 'react'
-import Title from '../components/Title'
-import { roomsDummyData } from '../assets/assets'
-import { useNavigate } from 'react-router'
-
-
+import React from "react";
+import Title from "../components/Title";
+import { assets, facilityIcons, roomsDummyData } from "../assets/assets";
+import { useNavigate } from "react-router";
+import StarRating from "../components/StarRating";
 
 const AllRooms = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
   return (
-    <div className='flex flex-col-reverse lg:flex-row items-start justify-between pt-28 md:pt-35 px-4 md:px-16 lg:px-24 xl:px-32'>
-        {/* Rooms */}
+    <div className="flex flex-col-reverse lg:flex-row items-start justify-between pt-28 md:pt-35 px-4 md:px-16 lg:px-24 xl:px-32">
+      {/* Rooms */}
+      <div>
         <div>
-            <div>
-                <Title title="Hotel Rooms" subTitle="Take advantage of our limited-time offers and special packages to enhance your stay and create unforgettable memories." font="font-playfair" align={"left"} />
-            </div>
-        
-        {roomsDummyData.map((room)=>(
-            <div>
-                <img className='max-h-65 md:w-1/2 rounded-xl shadow-xl shadow-lg object-cover cursor-pointer' onClick={() =>{ navigate(`/rooms/${room._id}`); window.scrollTo(0, 0)}} src={room.images[0]} alt="hotel-img" title='View Room Details' />
-            </div>
+          <Title
+            title="Hotel Rooms"
+            subTitle="Take advantage of our limited-time offers and special packages to enhance your stay and create unforgettable memories."
+            font="font-playfair"
+            align={"left"}
+          />
+        </div>
 
+        {roomsDummyData.map((room) => (
+          <div
+            key={room._id}
+            className="flex flex-col md:flex-row items-start py-10 gap-6 border-b border-gray-300 last:pb-30 last:border-0"
+          >
+            <img
+              className="max-h-65 md:w-1/2 rounded-xl shadow-xl shadow-lg object-cover cursor-pointer"
+              onClick={() => {
+                navigate(`/rooms/${room._id}`);
+                scrollTo(0, 0);
+              }}
+              src={room.images[0]}
+            />
+            <div className="md:w-1/2 flex flex-col gap-2">
+              <p className="text-gray-500">{room.hotel.city}</p>
+              <p
+                onClick={() => {
+                  navigate(`/rooms/${room._id}`);
+                  scrollTo(0, 0);
+                }}
+                className="text-gray-800 text-3xl font-playfair cursor-pointer"
+              >
+                {room.hotel.name}
+              </p>
+              <div className="flex items-center">
+                <StarRating rating={room.rating} />
+                <p className="ml-2">200+ reviews</p>
+              </div>
+              <div className="flex items-center gap-1 text-gray-500 mt-2 text-sm">
+                <img src={assets.locationIcon} alt="" />
+                <span className="ml-1">{room.hotel.address}</span>
+              </div>
+              <div className="flex flex-wrap items-center gap-4 mt-3 mb-6">
+                {room.amenities.map((item, index) => (
+                  <div
+                    key={index}
+                    className="flex items-center gap-2 px-3 py-2 rounded-lg bg-[#F5F5FF]/70"
+                  >
+                    <img
+                      src={facilityIcons[item]}
+                      alt={item}
+                      className="w-5 h-5"
+                    />
+                    <p className="text-xs">{item}</p>
+                  </div>
+                ))}
+              </div>
+              <p className="text-xl font-medium text-gray-700">
+                ${room.pricePerNight}/night
+              </p>
+            </div>
+          </div>
         ))}
+      </div>
+
+      {/* Filters */}
+      <div>
         
-
-        </div>
-        {/* Filters */}
-        <div>
-
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AllRooms
+export default AllRooms;
