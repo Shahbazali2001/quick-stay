7548d7a8fb7042b728690c53c177ff8aeab6379d
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-07-20 04:56:54 +0500

    New Route Added for My Bookings

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 
 import AllRooms from "./pages/AllRooms";
 import RoomDetails from "./pages/RoomDetails";
+import MyBookings from "./pages/MyBookings";
 
 function App() {
   const isOwnerPath = useLocation().pathname.includes("owner");
@@ -17,6 +18,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/rooms" element={<AllRooms />} />
           <Route path="/rooms/:id" element={<RoomDetails />} />
+          <Route path="/my-bookings" element={<MyBookings />} />
         </Routes>
       </div>
       <Footer />  
