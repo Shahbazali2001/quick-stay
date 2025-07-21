01aee6ab2c5faf897602c90a2d1596fd4c7b5b53
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-07-21 05:38:16 +0500

    App Updated with new routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,12 @@ import Home from "./pages/Home";
 import AllRooms from "./pages/AllRooms";
 import RoomDetails from "./pages/RoomDetails";
 import MyBookings from "./pages/MyBookings";
+import Layout from "./pages/hotelOwner/Layout";
+import Dashboard from "./pages/hotelOwner/Dashboard";
+import AddRoom from "./pages/hotelOwner/AddRoom";
+import ListRoom from "./pages/hotelOwner/ListRoom";
+
+
 
 
 function App() {
@@ -16,13 +22,19 @@ function App() {
   return (
    <div>
       {!isOwnerPath && <Navbar />}
-      { false && <HotelReg />}
+      { false && <HotelReg /> }
       <div className="min-h-[70vh]">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/rooms" element={<AllRooms />} />
           <Route path="/rooms/:id" element={<RoomDetails />} />
           <Route path="/my-bookings" element={<MyBookings />} />
+          <Route path="/owner" element={<Layout />}>
+              <Route index element={ <Dashboard />} />
+              <Route path="add-room" element={ <AddRoom />} />
+              <Route path="list-room" element={ <ListRoom />} />
+          </Route>
+
         </Routes>
       </div>
       <Footer />  
