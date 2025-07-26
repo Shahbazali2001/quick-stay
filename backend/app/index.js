551afc4901d83b06f5ee188129ea195539d30a2c
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-07-26 20:10:42 +0500

    adding app in the folder

---
+++ backend/app/index.js
@@ -0,0 +1,45 @@
+import express from "express";
+import cors from "cors";
+import { clerkMiddleware } from '@clerk/express'
+import "dotenv/config";
+
+
+// Controllers
+import clerkWebhooks from "../controllers/clerkWebhooks.js";
+
+import connectDB from "../configs/dbConnect.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Database Connection
+connectDB();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(clerkMiddleware());
+
+
+
+//API to Listen Clerk WebHooks
+app.use("/api/clerk", clerkWebhooks)
+
+
+
+
+
+
+
+
+// Root Route (Optional)
+app.get("/", (req, res) => {
+    res.send("Hello from QuickStay");
+});
+
+
+
+
+
+
+export default app;
