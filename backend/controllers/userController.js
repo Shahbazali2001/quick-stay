11004e589bd740ee5fd4e18c626a35457d4eee01
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-07-26 22:46:39 +0500

    adding store recent searched cities

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -11,3 +11,24 @@ export const getUserData = async (req, res) => {
         return res.status(500).json({success: false, message: error.message});
     }
 };
+
+
+// Store user Recent Searched Cities
+export const storeRecentSearchedCities = async (req, res) => {
+    try{
+
+        const {recentSearchedCity} = req.body;
+        const user = req.user;
+        if(user.recentSearchedCities < 3){
+            user.recentSearchedCities.push(recentSearchedCity);
+        }else{
+            user.recentSearchedCities.shift();
+            user.recentSearchedCities.push(recentSearchedCity);
+        }
+        await user.save();
+        return res.status(200).json({success: true, message: "User Data Updated Successfully"});
+
+    }catch(error){
+        return res.status(500).json({success: false, message: error.message});
+    }
+}
