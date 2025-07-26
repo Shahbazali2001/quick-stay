9f90e777a9281176932a62ee178bbe34b9bd2d20
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-07-27 02:14:42 +0500

    added user routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getUserData } from "../controllers/userController.js";
+import { getUserData, storeRecentSearchedCities } from "../controllers/userController.js";
 import { protect } from "../middlewares/authMiddleware.js";
 
 const userRouter = express.Router();
 
 userRouter.get("/", protect, getUserData);
+userRouter.post("/store-recent-searched-cities", protect, storeRecentSearchedCities);
 
 export default userRouter;
