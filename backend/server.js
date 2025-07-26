a1af898b0933d9bb314cf21c044185047b93c1ae
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-07-26 20:16:05 +0500

    adding server file with updates

--- backend/server.js
+++ backend/server.js
@@ -1,48 +1,8 @@
-import express from "express";
-import cors from "cors";
-import { clerkMiddleware } from '@clerk/express'
-import "dotenv/config";
+// local.js
+import app from "./app/index.js";
 
-
-// Controllers
-import clerkWebhooks from "./controllers/clerkWebhooks.js";
-
-import connectDB from "./configs/dbConnect.js";
-
-const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Database Connection
-connectDB();
-
-// Middlewares
-app.use(cors());
-app.use(express.json());
-app.use(clerkMiddleware());
-
-
-
-//API to Listen Clerk WebHooks
-app.use("/api/clerk", clerkWebhooks)
-
-
-
-
-
-
-
-
-// Root Route (Optional)
-app.get("/", (req, res) => {
-    res.send("Hello from QuickStay");
-});
-
-
-
-
-
-
-// Start Server
 app.listen(PORT, () => {
-    console.log("Server is running on port 3000");
+  console.log(`Server running locally on http://localhost:${PORT}`);
 });
