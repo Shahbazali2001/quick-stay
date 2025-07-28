3273ab8ef8574d6c3b909818a036663a11f9bde5
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-07-29 02:46:14 +0500

    Updated hotel registration form

--- frontend/src/components/HotelReg.jsx
+++ frontend/src/components/HotelReg.jsx
@@ -1,62 +1,149 @@
-import React from 'react'
+import { useState } from "react";
 import { assets, cities } from "../assets/assets";
+import { useAppContext } from "../context/useAppContext";
+import { toast } from "react-hot-toast";
 
 const HotelReg = () => {
+
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [contact, setContact] = useState("");
+  const [city, setCity] = useState("");
+
+  const { setShowHotelReg, axiosInstance, getToken, setIsOwner } =
+    useAppContext();
+
+  const onSubmitHandler = async (e) => {
+    try {
+      e.preventDefault();
+      const { data } = await axiosInstance.post(
+        "/api/hotels/",
+        { name, address, contact, city },
+        {
+          headers: {
+            Authorization: `Bearer ${await getToken()}`,
+          },
+        }
+      );
+
+      if (data.success) {
+        toast.success(data.message);
+        setIsOwner(true);
+        setShowHotelReg(false);
+      }else{
+        toast.error(data.message);
+      }
+
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
   return (
-    <div className='fixed top-0 bottom-0 left-0 right-0 z-100 flex items-center justify-center bg-black/70'>
-        <form className='flex bg-white rounded-xl max-w-4xl max-md:mx-2'>
-            {/* Image */}
-            <img src={assets.regImage} alt="" className='w-1/2 rounded-xl hidden md:block' />
+    <div
+      onClick={() => setShowHotelReg(false)}
+      className="fixed top-0 bottom-0 left-0 right-0 z-100 flex items-center justify-center bg-black/70"
+    >
+      <form
+        onSubmit={onSubmitHandler}
+        onClick={(e) => e.stopPropagation()}
+        className="flex bg-white rounded-xl max-w-4xl max-md:mx-2"
+      >
+        {/* Image */}
+        <img
+          src={assets.regImage}
+          alt=""
+          className="w-1/2 rounded-xl hidden md:block"
+        />
 
-            {/* Form */}
-            <div className='relative flex flex-col items-center md:w-1/2 p-8 md:p-10'>
+        {/* Form */}
+        <div className="relative flex flex-col items-center md:w-1/2 p-8 md:p-10">
+          <img
+            onClick={() => setShowHotelReg(false)}
+            src={assets.closeIcon}
+            alt=""
+            className="absolute top-4 right-4 h-4 w-4 cursor-pointer"
+          />
 
-                <img src={assets.closeIcon} alt="" className='absolute top-4 right-4 h-4 w-4 cursor-pointer' />
+          <p className="text-2xl font-semibold mt-6">Register Your Hotel</p>
 
-                <p className='text-2xl font-semibold mt-6'>Register Your Hotel</p>
+          {/* Hotel Name */}
+          <div className="w-full mt-4">
+            <label htmlFor="name" className="font-medium text-gray-500">
+              Hotel Name
+            </label>
+            <input
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="border border-gray-300 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light"
+              type="text"
+              placeholder="Hotel Name"
+              name="name"
+              id="name"
+              required
+            />
+          </div>
+          {/* Phone Number */}
+          <div className="w-full mt-4">
+            <label htmlFor="contact" className="font-medium text-gray-500">
+              Phone
+            </label>
+            <input
+              value={contact}
+              onChange={(e) => setContact(e.target.value)}
+              className="border border-gray-300 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light"
+              type="text"
+              placeholder="Phone"
+              name="contact"
+              id="contact"
+              required
+            />
+          </div>
+          {/* Address */}
+          <div className="w-full mt-4">
+            <label htmlFor="address" className="font-medium text-gray-500">
+              Address
+            </label>
+            <input
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+              className="border border-gray-300 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light"
+              type="text"
+              placeholder="Address"
+              name="address"
+              id="address"
+              required
+            />
+          </div>
+          {/* City Dropdown */}
+          <div className="w-full mt-4 max-w-60 mr-auto">
+            <label htmlFor="city" className="font-medium text-gray-500">
+              City
+            </label>
+            <select
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+              className="border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light"
+              name="city"
+              id="city"
+              required
+            >
+              <option value="">Select City</option>
+              {cities.map((city, index) => (
+                <option key={index} value={city}>
+                  {city}
+                </option>
+              ))}
+            </select>
+          </div>
 
-                {/* Hotel Name */}
-                <div className='w-full mt-4'>
-                  <label htmlFor="name" className='font-medium text-gray-500'>
-                    Hotel Name
-                  </label>
-                  <input className='border border-gray-300 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light' type="text" placeholder='Hotel Name' name="name" id="name" required />
-                </div>
-                {/* Phone Number */}
-                <div className='w-full mt-4'>
-                  <label htmlFor="contact" className='font-medium text-gray-500'>
-                    Phone
-                  </label>
-                  <input className='border border-gray-300 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light' type="text" placeholder='Phone' name="contact" id="contact" required />
-                </div>
-                {/* Address */}
-                <div className='w-full mt-4'>
-                  <label htmlFor="address" className='font-medium text-gray-500'>
-                    Address
-                  </label>
-                  <input className='border border-gray-300 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light' type="text" placeholder='Address' name="address" id="address" required />
-                </div>
-                {/* City Dropdown */}
-                <div className='w-full mt-4 max-w-60 mr-auto'>
-                  <label htmlFor="city" className='font-medium text-gray-500'>
-                    City
-                  </label>
-                  <select className='border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light' name="city" id="city" required>
-                    <option value="">Select City</option>
-                    {cities.map((city, index) => (
-                      <option key={index} value={city}>{city}</option>
-                    ))}
-                  </select>
-                </div>
-
-                <button className='bg-indigo-600 hover:bg-indigo-600 text-white transition-all mr-auto py-2 px-6 rounded cursor-pointer mt-6'>
-                  Register
-                </button>
-        
-            </div>
-        </form>
+          <button className="bg-indigo-600 hover:bg-indigo-600 text-white transition-all mr-auto py-2 px-6 rounded cursor-pointer mt-6">
+            Register
+          </button>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default HotelReg
+export default HotelReg;
